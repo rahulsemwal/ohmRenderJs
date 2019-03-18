4a909c353d24d6816fa38970aec54aba7686c7a9
Rahul Semwal <semwalr@HCL.COM>
2019-03-18 18:44:14 +0530

    ohmRenderjs lib changes

--- js/ohmRenderLib.js
+++ js/ohmRenderLib.js
@@ -29,6 +29,10 @@ UIengine2 = function(tpl, obj, cb) {
             },
             endCode = function() {
                 code = code + 'return r.join("");';
+            },
+            runCode = function(parsedCode,obj){
+              parsedCode = parsedCode.replace(/[\r\t\n]/g, '');
+              return new Function(parsedCode).call(obj);
             };
         startCode();
         while (match = re.exec(tpl)) {
@@ -44,32 +48,59 @@ UIengine2 = function(tpl, obj, cb) {
         if (typeof cb == "function") {
             cb(tpl);
         }
-        return tpl;
+        return runCode(code,obj);
+        // return new Function(code.replace(/[\r\t\n]/g, '')).apply(obj);
+        // return tpl;
     }
-    /*var tpl = '<p>Hi, my name is {{name}}, and my age is {{age}}, living in {{address}}.</p>';
-    var re = /{{([^}}]+)?}}/g,
-        match; //focusing {{}} custom literal
-    var cursor = 0,
-        code,
-        var startCode = function() {
-            code = 'var r = [];\n';
-        }
-    var addCode = function(chunk) {
-        code = code + 'r.push("' + chunk.replace(/"/g, '\\"') + '");\n'
-    };
-    var endCode = function() {
-        code = code + 'return r.join("");';
-    }
-    startCode();
-    while (match = re.exec(tpl)) {
-        addCode(tpl.slice(cursor, match.index));
-        addCode(match[1]);
-        cursor = match.index + match[0].length;
-    }
-    addCode(tpl.substr(cursor, tpl.length - cursor));
-    endCode();
-    console.log(code);*/
-UIengine2('<p>Hi, my name"s is {{name}}, and my age is {{age}}, living in {{address}}.</p>', {
+    
+UIengine2('<p>Hi, my name"s is {{name}}, and my age is {{age}}, living in {{address.street}}.</p>', {
     name: "rahul",
-    age: 23
+    age: 23,
+    address:{street:'Noida lan no 1'}
+});
+
+/*@DOC:By different method*/
+UIengine3 = function(tpl, obj, cb) {
+        //magic go here.
+        var re = /{{([^}}]+)?}}/g,
+            cursor = 0,
+            code,
+            startCode = function() {
+                code = 'var r = [];\n';
+            },
+            addCode = function(chunk, jsCode) {
+                jsCode ? (code = code + 'r.push(' + chunk + ');\n') :
+                    (code = code + 'r.push("' + chunk.replace(/"/g, '\\"') + '");\n');
+            },
+            endCode = function() {
+                code = code + 'return r.join("");';
+            },
+            runCode = function(parsedCode,obj){
+              parsedCode = parsedCode.replace(/[\r\t\n]/g, '');
+              return new Function(parsedCode).call(obj);
+            };
+        startCode();
+        while (match = re.exec(tpl)) {
+            addCode(tpl.slice(cursor, match.index));
+            // addCode(match[1], true);
+            addCode('this.' + match[1], true);
+            cursor = match.index + match[0].length;
+        }
+        addCode(tpl.substr(cursor, tpl.length - cursor));
+        endCode();
+        console.log(code, typeof code);
+        //magic end here.
+        if (typeof cb == "function") {
+            cb(tpl);
+        }
+        return runCode(code,obj);
+        // return new Function(code.replace(/[\r\t\n]/g, '')).apply(obj);
+        // return tpl;
+    }
+    
+UIengine2('{{this[0]}}<p>Hi, my name"s is {{name}}, and my age is {{age}}, living in {{address.street}}.</p>', {
+    '0':'first tupple',
+    name: "rahul",
+    age: 23,
+    address:{street:'Noida lan no 1'}
 });
